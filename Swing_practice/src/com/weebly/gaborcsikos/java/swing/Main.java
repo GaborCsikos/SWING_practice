a2d2491dd1b58b84dddf07818552bf7317075acb
Gabor Csikos <csiki123@gmail.com>
2016-02-27 11:36:38 +0100

    first commit

---
+++ Swing_practice/src/com/weebly/gaborcsikos/java/swing/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.weebly.gaborcsikos.java.swing;
+
+/**
+ * @author Gabor Csikos
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
